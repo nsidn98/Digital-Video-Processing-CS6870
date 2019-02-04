6b2db8106f6f1dfecadeb3f195f2a699d96c0672
Siddharth Nayak <siddharthnayak98@gmail.com>
2019-02-05 01:16:10 +0530

    Working Background Subtraction

--- Assignment1/GMM.py
+++ Assignment1/GMM.py
@@ -1,8 +1,38 @@
+'''
+-----------------------------------------
+Implementation of
+Adaptive Background Mixture Models for Real-Time Tracking
+by
+Chris Stauffer & W.E.L. Grimson
+
+Background Subtraction using Gaussian Mixture Models
+~~~
+Author: Siddharth Nayak
+~~~
+-----------------------------------------
+'''
+
+
 import numpy as np
 import cv2
 import matplotlib.pyplot as pyplot
+from tqdm import tqdm
 
-vidcap = cv2.VideoCapture('Videos/Run.avi')
+K = 5 # number of gaussian components
+std_thresh = 2.5 # number of background components
+alpha = 0.05 # learning rate
+std_init = 10 # initialise std matrix
+height = 144
+width = 180
+channels = 3
+foreground = np.zeros((height,width))
+W = (1/K)*np.ones((height, width, K)) # weights
+mu = np.random.randint(0,255,(height, width, channels, K)) # initialise pixel means [h,w,c,K]
+sigma = std_init * np.ones((height,width,channels,K)) # initialise pixel stds [h,w,c,K]
+T = 0.7
+fg = np.zeros((height,width))
+
+vidcap = cv2.VideoCapture('Assignment1/Videos/Run.avi')
 success,image = vidcap.read()
 count = 0
 frames=[]
@@ -10,105 +40,88 @@ while success:
     frames.append(image)
     success,image = vidcap.read()
     count += 1
+    
+vidcap.release()
 
-
-  
-class GMM():
-    def __init__(self,frames,height,width,gaussian_no=3,background_comp=3,std_thresh=2.5,alpha=0.01,thresh=0.25,std_init=10):
-        self.gaussian_no = gaussian_no # typically 3-5 as mentioned in paper
-        self.background_comp = background_comp # number of background components
-        self.std_thresh = std_thresh # standard deviation threshold:2.5 as mentioned in paper
-        self.alpha = alpha # learning rate (between 0 and 1) (from paper 0.01)
-        self.thresh = thresh # foreground threshold (0.25 or 0.75 in the paper)
-        self.std_init = 10 # initial standard deviation for new components
-        self.height = height
-        self.width = width
-        self.W = (1/self.gaussian_no) * np.ones((self.height,self.width,self.gaussian_no)) # initialise the weight array
-        self.mean = np.random.randint(0,255,(self.height,self.width,self.gaussian_no)) # initialise pixel means
-        self.std = self.std_init * np.ones((self.height,self.width,self.gaussian_no)) # initialise pixel standard deviations
-        #self.u_diff = np.zeros((self.height,self.width,self.gaussian_no)) #difference of each pixel from mean
-        self.background = np.zeros((self.height,self.width))
-        self.frames = frames
+def process(frames):
+    count = 0
+    
+    f=[]
+    for fr in tqdm(range(len(frames))):
+        fg = np.zeros((height,width))
+        frame = frames[fr]
+        mu_diff = np.abs(np.expand_dims(frame,axis=3)-mu)
         
-    def process(self):
-        fg_arr=[]
-        # frames is array [height,width,channels,frames]
-        fg = np.zeros((self.height, self.width));
-        
-        frame_no = 0
-        for frame in self.frames:
-            
-            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-            # r_frame = frame[:,:,0]
-            # g_frame = frame[:,:,1]
-            # b_frame = frame[:,:,2]
-            #
-            
-            u_diff = np.abs(np.expand_dims(frame,axis=2)-self.mean)
-            # u_diff_g = np.abs(g_frame-self.mean)
-            # u_diff_b = np.abs(b_frame-self.mean)
-            
-            # update gaussian for each pixel
-            for h in range(self.height):
-                for w in range(self.width):
-                    match = 0
-                    for g in range(self.gaussian_no):
-                        if np.abs(u_diff[h,w,g]) <= self.background_comp * self.std[h,w,g]:
-                            match = 1 # flag for indicating signal matches
-                            # update weights, mean, std, p
-                            self.W[h,w,g] = (1-self.alpha)*self.W[h,w,g] + self.alpha  # equation 5 in paper
-                            # np.dot((),np.linalg.inv(self.std))
-                            p = self.alpha * (np.exp(-0.5*()))
-                            self.mean[h,w,g] = (1-self.p)*self.mean[h,w,g] + self.p*frame[h,w] # equation 6 in paper
-                            self.std[h,w,g] = np.sqrt((1-self.p) * self.std[h,w,g]**2 + self.p * (frame[h,w] - self.mean[h,w,g])**2) # equation 7 in paper
-                        else : # pixel does not match
-                            self.W[h,w,g] = (1-self.alpha) * self.W[h,w,g]
-                            
-                    self.W[h,w,:] = self.W[h,w,:]/np.sum(self.W[h,w,:])
-                    
-                    self.background[h,w] = 0
-                    for g in range(self.gaussian_no):
-                        self.background = self.background + self.mean[h,w,g] * self.W[h,w,g]
+        for i in (range(height)):
+            for j in range(width):
+                match = -1
+                for k in range(K):
+                    if abs(np.linalg.norm(mu_diff[i,j,:,k]))<= std_thresh * sigma[i,j,:,k][0]:
+                        match = k
+                        # update weights
+                        W[i,j,k] = (1-alpha)*W[i,j,k] + alpha  # equation 5 in paper
+                        sigma_matrix = sigma[i,j,:,k]**2 * np.eye(3) # sigma^2.I
+                        sigma_inv = np.linalg.pinv(sigma_matrix)
+                        # equation 3 in paper
+                        eta = (0.5/np.pi)**(3/2)*(1/np.linalg.det(sigma_matrix)**0.5)*np.exp(-0.5*np.matmul((np.matmul(frame[i,j,:]-mu[i,j,:,k],sigma_inv)),frame[i,j,:]-mu[i,j,:,k]))
+                        p = alpha * eta # equation 8
+                        mu[i,j,:,k] = (1-p)*mu[i,j,:,k] + p*(frame[i,j,:]) # equation 6 in paper
+                        sigma[i,j,:,k] = np.sqrt((1-p) * sigma[i,j,:,k]**2 + p * (np.matmul(frame[i,j,:] - mu[i,j,:,k] , frame[i,j,:] - mu[i,j,:,k]))) # equation 7 in paper
                         
-                    # if no components match them new component
-                    if match == 0:
-                        min_index = np.argmin(self.W[h,w,:])
-                        min_weight = np.min(self.W[h,w,:])
-                        self.mean[h,w,min_index] = frame[h,w]
-                        self.std[h,w,min_index] = self.std_init
-                    
-                    rank = self.W[h,w,:]/self.std[h,w,:] # calculate the rank of components
-                    rank_ind = np.linspace(0,self.gaussian_no-1,self.gaussian_no)
-                    for k in range(1,len(rank_ind)):
-                        for m in range(k-1):
-                            if rank[k]>rank[m]:
-                                rank_temp = rank[m]
-                                rank[m] = rank[k]
-                                rank[k] = rank_temp
-                                
-                                rank_ind_temp = rank_ind[m]
-                                rank_ind[m] = rank_ind[k]
-                                rank_ind[k] = rank_ind_temp
-                    
-                    match = 0
-                    k = 0
-                    fg[h,w] = 0
-                    while ((match == 0)&(k<self.background_comp)):
-                        if float(self.W[h,w,int(rank_ind[k])]) >= float(self.thresh):
-                            if float(np.abs(u_diff[h,w,int(rank_ind[k])])) <= float(self.std_thresh*self.std[h,w,int(rank_ind[k])]):
-                                fg[h,w] = 0
-                                
-                                match = 1
-                            else:
-                                fg[h,w] = frame[h,w]
-                                
-                        k+=1
-                    
-            fg_arr.append(fg)
-            frame_no+=1
-            print(frame_no)
-        return fg_arr
-gmm = GMM(frames,144,180)
-f = gmm.process()
-            
+                    else:
+                        W[i,j,k] = (1-alpha)*W[i,j,k]
+                        
+                if match == -1:
+                    min_index = np.argmin(W[i,j,:])
+                    min_weight = np.min(W[i,j,:])
+                    mu[i,j,:,min_index] = frame[i,j,:]
+                    sigma[i,j,:,min_index] = std_init
                 
+                # rank the components according to W/sigma
+                rank = W[i,j,:]/sigma[i,j,0,:]
+                rank_ind =np.argsort(rank)[::-1]
+                sum_W = 0
+                bg_index = []
+                # equation 9 in paper
+                for l in range(K):
+                    sum_W += W[i,j,rank_ind[l]]
+                    bg_index.append(rank_ind[l])
+                    if sum_W > T:
+                        break
+                if len(bg_index)== K:
+                    fg[i,j] = 1
+                    
+                elif match in bg_index:
+                    fg[i,j] = 0
+                else:
+                    fg[i,j] = 1
+        count += 1
+        f.append(fg)
+    return f
+    
+f = process(frames)
+
+
+for i in range(len(f)):
+    
+    cv2.imshow('df',f[i])
+    cv2.waitKey(40)
+    # g = cv2.cvtColor(f[i],cv2.COLOR_GRAY2BGR)
+    # out.write(np.expand_dims(g,axis=2))
+
+
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (width,height))
+
+for i in range(len(f)):
+    
+    g = cv2.cvtColor(f[i].astype(np.float32),cv2.COLOR_GRAY2BGR)
+        # write the flipped frame
+    out.write(np.uint8(g))
+
+    
+
+# Release everything if job is finished
+out.release()
--- Assignment1/GMM_sub.py
+++ Assignment1/GMM_sub.py
@@ -16,46 +16,96 @@ Author: Siddharth Nayak
 import numpy as np
 import cv2
 import matplotlib.pyplot as pyplot
+from tqdm import tqdm
 
 K = 5 # number of gaussian components
 std_thresh = 2.5 # number of background components
-alpha = 0.01 # learning rate
+alpha = 0.05 # learning rate
 std_init = 10 # initialise std matrix
 height = 144
 width = 180
 channels = 3
+foreground = np.zeros((height,width))
 W = (1/K)*np.ones((height, width, K)) # weights
 mu = np.random.randint(0,255,(height, width, channels, K)) # initialise pixel means [h,w,c,K]
 sigma = std_init * np.ones((height,width,channels,K)) # initialise pixel stds [h,w,c,K]
+T = 0.7
+fg = np.zeros((height,width))
+
+vidcap = cv2.VideoCapture('Assignment1/Videos/Run.avi')
+success,image = vidcap.read()
+count = 0
+frames=[]
+while success:
+    frames.append(image)
+    success,image = vidcap.read()
+    count += 1
+    
+vidcap.release()
 
 def process(frames):
+    count = 0
+    
+    f=[]
     for frame in frames:
+        print(count)
+        fg = np.zeros((height,width))
         mu_diff = np.abs(np.expand_dims(frame,axis=3)-mu)
         
-        for i in range(height):
+        for i in tqdm(range(height)):
             for j in range(width):
-                match = 0
+                match = -1
                 for k in range(K):
-                    if abs(np.linalg.norm(mu_diff[i,j,:,k])<= std_thresh * sigma[i,j,:,k][0]):
-                        match = 1
+                    if abs(np.linalg.norm(mu_diff[i,j,:,k]))<= std_thresh * sigma[i,j,:,k][0]:
+                        match = k
                         # update weights
                         W[i,j,k] = (1-alpha)*W[i,j,k] + alpha  # equation 5 in paper
                         sigma_matrix = sigma[i,j,:,k]**2 * np.eye(3) # sigma^2.I
                         sigma_inv = np.linalg.pinv(sigma_matrix)
                         # equation 3 in paper
-                        eta = (0.5/np.pi)**(K/2)*(1/np.linalg.det(sigma_matrix))*np.exp(-0.5*np.dot((np.dot(frame[i,j,:]-mu[i,j,:,k],sigma_inv)),frame[i,j,:]-mu[i,j,:,k]))
+                        eta = (0.5/np.pi)**(3/2)*(1/np.linalg.det(sigma_matrix)**0.5)*np.exp(-0.5*np.matmul((np.matmul(frame[i,j,:]-mu[i,j,:,k],sigma_inv)),frame[i,j,:]-mu[i,j,:,k]))
                         p = alpha * eta # equation 8
-                        mu[i,j,k] = (1-p)*mu[i,j,k] + p*frame[h,w,:] # equation 6 in paper
-                        sigma[i,j,:,k] = np.sqrt((1-p) * sigma[i,j,k]**2 + p * (np.dot(frame[i,j,:] - mean[i,j,:,k] , frame[i,j,:] - mean[i,j,:,k])) # equation 7 in paper
+                        mu[i,j,:,k] = (1-p)*mu[i,j,:,k] + p*(frame[i,j,:]) # equation 6 in paper
+                        sigma[i,j,:,k] = np.sqrt((1-p) * sigma[i,j,:,k]**2 + p * (np.matmul(frame[i,j,:] - mu[i,j,:,k] , frame[i,j,:] - mu[i,j,:,k]))) # equation 7 in paper
+                        
                     else:
                         W[i,j,k] = (1-alpha)*W[i,j,k]
                         
-                if match = 0:
+                if match == -1:
                     min_index = np.argmin(W[i,j,:])
                     min_weight = np.min(W[i,j,:])
-                    mu[h,w,:,min_index] = frame[i,j,:]
+                    mu[i,j,:,min_index] = frame[i,j,:]
                     sigma[i,j,:,min_index] = std_init
-
-
-
+                
+                # rank the components according to W/sigma
+                rank = W[i,j,:]/sigma[i,j,0,:]
+                rank_ind =np.argsort(rank)[::-1]
+                sum_W = 0
+                bg_index = []
+                # equation 9 in paper
+                for l in range(K):
+                    sum_W += W[i,j,rank_ind[l]]
+                    bg_index.append(rank_ind[l])
+                    if sum_W > T:
+                        break
+                if len(bg_index)== K:
+                    fg[i,j] = 1
+                    
+                elif match in bg_index:
+                    fg[i,j] = 0
+                else:
+                    fg[i,j] = 1
+        count += 1
+        f.append(fg)
+    return f
     
+f = process(frames)
+
+
+for i in range(len(f)):
+    
+    cv2.imshow('df',f[i])
+    cv2.waitKey(40)
+    # g = cv2.cvtColor(f[i],cv2.COLOR_GRAY2BGR)
+    # out.write(np.expand_dims(g,axis=2))
+
